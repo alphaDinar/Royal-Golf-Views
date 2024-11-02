1b0da3aefc656b7a9af2a693707855d8809d5ba0
cathsuchurch <yanksone18@yahoo.com>
2024-11-02 12:33:02 +0000

    'change'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id="google-tag-head"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-YC7WEMGQXB`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-YC7WEMGQXB', {
+                page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
+      <head />
       <body className={inter.className}>{children}</body>
     </html>
   )
